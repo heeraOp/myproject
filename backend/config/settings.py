68c2368c7ad4c6af295f6dbf5002d8f79a62fa4f
heeraWahengbam <iamalldestitute@gamil.com>
2026-02-06 22:14:03 +0530

    Frontend adminlogin + ngrok backend integration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,6 +20,15 @@ DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://panthoibihousie.netlify.app",
+    "https://intangibly-descendible-fransisca.ngrok-free.dev",
+]
+
+
+
 # --------------------------------------------------
 # APPLICATIONS
 # --------------------------------------------------
